9fa088e246f9d7a179383c5e2a2522197d87c44a
whitechicha <115341615+whitechicha@users.noreply.github.com>
2024-03-05 00:42:32 +0300

    MainComponents.vue

--- src/components/MainComponents.vue
+++ src/components/MainComponents.vue
@@ -1,7 +1,31 @@
 <template>
     <main>
         <div class="main-header">
-            
+            <div class="button-main-catalog">
+                <a href="#">Woman's Fashion   <b>></b> </a>
+                <a href="#">Men's Fashion   <b>></b> </a>
+                <a href="#">Electronics</a>
+                <a href="#">Home & Lifestyle</a>
+                <a href="#">Medicine</a>
+                <a href="#">Sports & Outdoor</a>
+                <a href="#">Baby's & Toys</a>
+                <a href="#">Groceries & Pets</a>
+                <a href="#">Health & Beauty</a>
+            </div>
         </div>
     </main>
 </template>
+
+<style scoped>
+
+.button-main-catalog {
+    display: flex;
+    flex-direction: column;
+}
+
+.button-main-catalog a {
+    text-decoration: none;
+    color: black;
+    padding: 5px;
+}
+</style>
