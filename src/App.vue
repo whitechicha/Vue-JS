d891ddf2bf6ec5f089e5c69117a6f8fb7af5c206
whitechicha <115341615+whitechicha@users.noreply.github.com>
2024-03-05 00:42:56 +0300

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <DropDown />
+  <MainComponents />
 </template>
 
 <script setup>
 import DropDown from './components/DropDown.vue';
+import MainComponents from './components/MainComponents.vue';
 
 
 </script>
